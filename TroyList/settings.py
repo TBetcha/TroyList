53b354ad0a5ba6f40e02e689122811d7369997b3
Tbetcha <troy.boettger@gmail.com>
2020-05-09 03:14:57 -0400

    update styling

--- TroyList/settings.py
+++ TroyList/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'troys_list.apps.TroysListConfig',
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 
 ]
 
